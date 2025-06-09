72887f62d9b7d849881a16f982735872936a302d
zmykedev <zmaikol399@gmail.com>
2025-06-09 18:04:47 -0400

    type of url from railway

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = `https://${import.meta.env.VITE_RAILWAY_SERVICE_NAME}-${import.meta.env.VITE_RAILWAY_ENVIRONMENT_NAME}.up.railway.app`
 
 
 export const API_ENDPOINTS = {
